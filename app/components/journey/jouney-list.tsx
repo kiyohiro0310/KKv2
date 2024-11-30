f8b8066594718edc8f084e0c5f1df5fdc2a1e91f
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2024-11-30 21:36:35 +1100

    modify

--- app/components/journey/jouney-list.tsx
+++ app/components/journey/jouney-list.tsx
@@ -3,7 +3,7 @@ import JourneyListTable from './journey-list-table';
 
 const JourneyList = ({learnings, lifes}: {learnings: {}[]; lifes: {}[]}) => {
   return (
-    <div className='py-24 w-full md:w-2/3 space-y-4 md:space-y-0 md:space-x-4 mx-auto flex flex-col md:flex-row justify-around items-center'>
+    <div className='py-24 w-full md:w-2/3 space-y-4 md:space-y-0 md:space-x-4 mx-auto flex flex-col md:flex-row justify-around'>
         <JourneyListTable isLearning={true} title='Learning Journey' lists={learnings}/>
         <JourneyListTable isLearning={false} title='Life Journey' lists={lifes}/>
     </div>
