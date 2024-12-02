4e3a6cce35a7df0c76d7df8039eca61aff6c390c
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2024-12-02 23:53:20 +1100

    Modify

--- app/components/journey/journey-list-table.tsx
+++ app/components/journey/journey-list-table.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 const JourneyListTable = ({ title, lists, isLearning }: { title: string; lists: {}[]; isLearning: boolean;}) => {
   return (
-    <div className="w-1/2 space-y-10">
+    <div className="w-[80%] md:w-1/2 mx-auto space-y-10 py-6">
       <h1 className="font-bold text-2xl text-center pb-6">{title}</h1>
       {lists.map((item: any, index: number) => {
         return (
           <Link href={isLearning ? `/learning/${item._id}` : `/daily/${item._id}` } key={index}>
             <div
-              className="flex flex-col md:flex-row justify-start items-center space-x-6 overflow-hidden border-b-[0.5px] border-gray-300 py-4 hover:text-sub transition-all duration-200"
+              className="w-full flex flex-col md:flex-row justify-start items-center space-x-6 overflow-hidden border-b-[0.5px] border-gray-300 py-4 hover:text-sub transition-all duration-200"
             >
               <p className="w-full md:w-1/4 h-8 overflow-hidden text-sm flex items-center">{item.date}</p>
               <p className="w-full md:w-3/4 h-8 overflow-hidden">{item.title}</p>
