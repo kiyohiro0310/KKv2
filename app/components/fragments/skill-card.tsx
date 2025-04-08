90989fdc1655d5f7f94a7dafebc99f874c8654a0
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2025-04-08 17:16:13 +1000

    Modify layout

--- app/components/fragments/skill-card.tsx
+++ app/components/fragments/skill-card.tsx
@@ -17,7 +17,7 @@ const SkillCard = ({
   cardTitle: string
 }) => {
   return (
-    <div className={`relative w-full md:w-[23vw] h-fit md:h-[50vh] p-4 text-center flex flex-col items-center space-y-8 ${bg}`}>
+    <div className={`relative w-full md:w-[23vw] p-4 py-12 text-center flex flex-col items-center space-y-8 ${bg}`}>
       <div className="hidden md:block absolute -top-[25px] w-[50px] h-[50px] bg-black rotate-45"></div>
       <div className="hidden md:block absolute -top-[52px] w-[40px] h-[40px] bg-white rotate-45"></div>
       <div className="hidden md:block absolute -top-[42px] font-bold text-black text-sm">{cardTitle}</div>
