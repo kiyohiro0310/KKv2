5d108968dd9b1cc856664864f8e14b7825b2a51e
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2025-07-25 00:45:33 +1000

    Modify overflow

--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -1,7 +1,11 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React, { useEffect, useRef } from "react";
-import { fadeInUp, slideInRight, createScrollTrigger } from "@/app/lib/animations";
+import {
+  fadeInUp,
+  slideInRight,
+  createScrollTrigger,
+} from "@/app/lib/animations";
 import Image from "next/image";
 
 const Hero = () => {
@@ -10,7 +14,8 @@ const Hero = () => {
   const backgroundRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!contentRef.current || !imageRef.current || !backgroundRef.current) return;
+    if (!contentRef.current || !imageRef.current || !backgroundRef.current)
+      return;
 
     // Animate content
     const contentAnimation = fadeInUp(contentRef.current, 0.2);
@@ -36,23 +41,47 @@ const Hero = () => {
           Without haste, but without rest
         </p>
         <p className="my-6 text-sm md:text-base lg:text-lg max-w-2xl text-gray-500 dark:text-gray-400">
-          Have a blast on your journey—it&apos;s the one adventure that&apos;s all yours!
+          Have a blast on your journey—it&apos;s the one adventure that&apos;s
+          all yours!
         </p>
-        <Link href={"/contact"} className="px-8 py-3 bg-primary hover:bg-primary-dark font-medium text-lg rounded-lg shadow-lg transition-all duration-300 hover:text-main">
+        <Link
+          href={"/contact"}
+          className="px-8 py-3 bg-primary hover:bg-primary-dark font-medium text-lg rounded-lg shadow-lg transition-all duration-300 hover:text-main"
+        >
           Contact
         </Link>
-        <Link href={'https://aws.amazon.com/certification/certified-cloud-practitioner/'} className="pt-12 flex justify-start align-start">
-          <Image src="/images/clf.png" alt="clf logo" width={50} height={50} />
-        </Link>
-      </div>
-
-      <div ref={imageRef} className="relative md:w-1/2 flex justify-center mt-8 md:mt-0">
-        <div className="relative w-[40vw] h-[40vw] sm:w-[35vw] sm:h-[35vw] rounded-full overflow-hidden shadow-lg transform bg-hero bg-cover bg-center">
+        <div className="flex w-full pt-12 justify-center sm:justify-start">
+          <Link
+            href={
+              "https://aws.amazon.com/certification/certified-cloud-practitioner/"
+            }
+         
+          >
+            <Image
+              src="/images/clf.png"
+              alt="clf logo"
+              width={50}
+              height={50}
+            />
+          </Link>
         </div>
       </div>
 
-      <div ref={backgroundRef} className="hidden sm:block absolute -right-[5vw] -bottom-[13vw] rounded-[30%] w-[30vw] h-[30vw] bg-sub rotate-45"></div>
-      <div ref={backgroundRef} className="sm:hidden block absolute -right-[5vw] -bottom-[13vw] rounded-[30%] w-[200px] h-[200px] bg-sub rotate-45"></div>
+      <div
+        ref={imageRef}
+        className="relative md:w-1/2 flex justify-center mt-8 md:mt-0"
+      >
+        <div className="relative w-[40vw] h-[40vw] sm:w-[35vw] sm:h-[35vw] rounded-full overflow-hidden shadow-lg transform bg-hero bg-cover bg-center"></div>
+      </div>
+
+      <div
+        ref={backgroundRef}
+        className="hidden sm:block absolute -right-[5vw] -bottom-[13vw] rounded-[30%] w-[30vw] h-[30vw] bg-sub rotate-45"
+      ></div>
+      <div
+        ref={backgroundRef}
+        className="sm:hidden block absolute -right-[5vw] -bottom-[13vw] rounded-[30%] w-[200px] h-[200px] bg-sub rotate-45"
+      ></div>
     </div>
   );
 };
