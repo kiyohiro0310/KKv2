6e41588d1640ce40a0539f1af34fb03cef633a69
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2026-01-29 16:56:23 +1100

    Modify next config ts to hide secret

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  env: {
+    EMAILJS_SERVICE_ID: "service_1rakc4l",
+    EMAILJS_TEMPLATE_ID: "template_bvj0l5n",
+    EMAILJS_PUBLIC_KEY: "N_58djKIYO4a4t-D3",
+    MONGODB_URI: "mongodb+srv://Kiyohiro:DCCI9egxOCkN2cmI@cluster0.mvh0o.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=Cluster0"
+  }
+};
+
+export default nextConfig;
